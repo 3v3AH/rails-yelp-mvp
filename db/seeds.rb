eed194f10d052548781072fca1aea6aa40b72e28
Evelyn Aviles <eellkkmm@gmail.com>
2025-05-20 19:53:40 -0600

    Restaurant with MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create!(name: "Epicure", address: "Paris", phone_number: "1234567890", category: "french")
+puts "Restaurant number #{Restaurant.count} creado con exito"
+Restaurant.create!(name: "El Higueron", address: "Spain", phone_number: "34952119163", category: "french")
+puts "Restaurant number #{Restaurant.count} creado con exito"
+Restaurant.create!(name: "Ohlala", address: "Guadalajara", phone_number: "3324727784", category: "french")
+puts "Restaurant number #{Restaurant.count} creado con exito"
+Restaurant.create!(name: "Le Bernandin", address: "US", phone_number: "12125541515", category: "french")
+puts "Restaurant number #{Restaurant.count} creado con exito"
+Restaurant.create!(name: "Chilenazo", address: "Chile", phone_number: "56997288600", category: "french")
+puts "Restaurant number #{Restaurant.count} creado con exito"
